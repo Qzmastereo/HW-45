fe91ac09979d42f571a693ce77e0e78909a1c479
Volodymyr Udsianskiy <vv7827242@gmail.com>
2024-11-25 21:41:53 +0200

    ch

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// let count = 1;
+
+// while (count <= 10) {
+//     console.log(count);
+//     count += 1;
+// }
+
+// let number = 10;
+
+// for (let i = 2; i <= number; i += 1){
+//     if (i % 2 !== 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+//  const numbe = 7;
+ 
+
+// for (let i = 1; i <= 10; i++) {
+//     let table = '';
+//     table += `${numbe * i}`;
+//     console.log(table);
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// let i = 0;
+
+// while (i < numbers.length) {
+//     console.log(numbers[i]);
+//     i++; 
+// }
+
+// const massive = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < massive.length; i++) { 
+//     if (massive[i] === 7) { 
+//         break; 
+//     }
+//     console.log(massive[i]); 
+// }
+
+// let n = 20;  
+
+// for (let i = 0; i < n; i++) {  
+//     if (i >= n) {  
+//         break;  
+//     }
+//     console.log(i);  
+// }
