b5bc299ad84fefb5aecb11e242fa5b88df56e47c
Volodymyr Udsianskiy <vv7827242@gmail.com>
2024-11-26 16:36:20 +0200

    ch

--- js/main.js
+++ js/main.js
@@ -28,23 +28,44 @@
 
 // while (i < numbers.length) {
 //     console.log(numbers[i]);
-//     i++; 
+//     i++;
 // }
 
 // const massive = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// for (let i = 0; i < massive.length; i++) { 
-//     if (massive[i] === 7) { 
-//         break; 
+// for (let i = 0; i < massive.length; i++) {
+//     if (massive[i] === 7) {
+//         break;
 //     }
-//     console.log(massive[i]); 
+//     console.log(massive[i]);
 // }
 
-// let n = 20;  
+// let n = 20;
 
-// for (let i = 0; i < n; i++) {  
-//     if (i >= n) {  
-//         break;  
+// for (let i = 0; i < n; i++) {
+//     if (i >= n) {
+//         break;
 //     }
-//     console.log(i);  
+//     console.log(i);
 // }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+//Додаткове завдання
+let total = 0;
+
+while (true) {
+    let input = prompt("Введіть число чи натисніть Cancel для завершення");
+    if (input === null) {
+        break;
+    }
+
+    total += Number(input); 
+}
+
+alert(`Сума чисел ${total}`);
